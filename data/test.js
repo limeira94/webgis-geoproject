793eada75b551a2dd48a788c13680309236aed1f
Felipe Limeira <limeira.felipe94@gmail.com>
2023-11-07 12:59:22 -0300

    feat: add features to the frontend

---
+++ data/test.js
@@ -0,0 +1,135 @@
+var data = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": { "name": "São Paulo - SP, Centro" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.633308, -23.550520] // São Paulo - SP, Centro
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "Avenida Paulista" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.605095, -23.551963] // Avenida Paulista
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "Parque Ibirapuera" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.697710, -23.550130] // Parque Ibirapuera
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "Mercado Municipal" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.689838, -23.543178] // Mercado Municipal
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "Zoológico de São Paulo" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.720870, -23.584673] // Zoológico de São Paulo
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "Estádio do Pacaembu" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.637207, -23.528693] // Estádio do Pacaembu
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "Allianz Parque" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.658829, -23.559516] // Allianz Parque
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "Museu de Arte de São Paulo" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.702297, -23.546687] // Museu de Arte de São Paulo
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "Parque Villa Lobos" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.633383, -23.588089] // Parque Villa Lobos
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "Theatro Municipal" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.655442, -23.542763] // Theatro Municipal
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "Autódromo de Interlagos" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.727245, -23.587309] // Autódromo de Interlagos
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "Estádio Morumbi" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.693523, -23.591898] // Estádio Morumbi
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "Pinacoteca do Estado" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.634112, -23.545033] // Pinacoteca do Estado
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "São Paulo Expo" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.736746, -23.550508] // São Paulo Expo
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "Sambódromo do Anhembi" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.641621, -23.569108] // Sambódromo do Anhembi
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": { "name": "Shopping Ibirapuera" },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-46.729303, -23.531197] // Shopping Ibirapuera
+        }
+      },
+      // Adicione mais pontos conforme necessário
+    ]
+  }
+  
